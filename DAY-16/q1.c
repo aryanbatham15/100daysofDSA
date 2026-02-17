bc502690978752b6db02af3c1afacad4ba3f20e8
aryanbatham15 <aryanbatham15@gmail.com>
2026-02-17 11:34:28 +0530

    completed q1 of day(14,15,16,17)

---
+++ DAY-16/q1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, count;
+
+    scanf("%d", &n);
+
+    int arr[n], freq[n];
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        freq[i] = -1;  
+    }
+
+    for (i = 0; i < n; i++) {
+        if (freq[i] != -1)
+            continue;
+
+        count = 1;
+        for (j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+                freq[j] = 0; 
+            }
+        }
+        freq[i] = count;
+    }
+
+    for (i = 0; i < n; i++) {
+        if (freq[i] > 0) {
+            printf("%d:%d ", arr[i], freq[i]);
+        }
+    }
+
+    return 0;
+}
