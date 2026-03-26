3cf93ef5b1e7cf730cb728e71ecea0c5f229daed
aryanbatham15 <aryanbatham15@gmail.com>
2026-03-26 11:37:28 +0530

    completed day53

---
+++ DAY-53/q1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure for tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Find LCA
+struct Node* LCA(struct Node* root, int n1, int n2) {
+    if (root == NULL)
+        return NULL;
+
+    if (root->data == n1 || root->data == n2)
+        return root;
+
+    struct Node* left = LCA(root->left, n1, n2);
+    struct Node* right = LCA(root->right, n1, n2);
+
+    if (left && right)
+        return root;
+
+    return (left != NULL) ? left : right;
+}
+
+int main() {
+    // Constructing the example tree
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->right->left = newNode(6);
+    root->right->right = newNode(7);
+
+    int n1 = 4, n2 = 5;
+
+    struct Node* lca = LCA(root, n1, n2);
+
+    if (lca != NULL)
+        printf("LCA is %d\n", lca->data);
+    else
+        printf("LCA not found\n");
+
+    return 0;
+}
